e957704121d5e2064a0942b6e7669a638349997d
Isac Marín Sirias <isacms11@gmail.com>
2022-03-28 15:53:37 -0600

    Clase Targeta
    Confeccion de la clase targeta en c++

---
+++ CppServer/tarjeta.cpp
@@ -0,0 +1,79 @@
+#include "tarjeta.h"
+#include <string>
+
+
+tarjeta::tarjeta(int pos_i, int pos_j, int id, int memory){
+    this->pos_i = pos_i;
+    this->pos_j = pos_j;
+    this-> id = id;
+    this->memory = memory;
+}
+
+void tarjeta::obtener_imagen(int id){
+
+    switch (id)
+    {
+    case 1:
+
+        this->imgprovisional = "Numero1.png";
+        break;
+    
+    case 2:
+
+        this->imgprovisional = "Numero2.png";
+        break;
+    
+    case 3:
+
+        this->imgprovisional = "Numero3.png";
+        break;
+    
+    case 4:
+
+        this->imgprovisional = "Numero4.png";
+        break;
+    
+    case 5:
+
+        this->imgprovisional = "Numero5.png";
+        break;
+    
+    case 6:
+
+        this->imgprovisional = "Numero6.png";
+        break;
+    
+    case 7:
+
+        this->imgprovisional = "Numero7.png";
+        break;
+    
+    case 8:
+
+        this->imgprovisional = "Numero8.png";
+        break;
+    
+    case 9:
+
+        this->imgprovisional = "Numero9.png";
+        break;
+    
+    case 10:
+
+        this->imgprovisional = "Numero10.png";
+        break;
+    
+    case 11:
+
+        this->imgprovisional = "Numero11.png";
+        break;
+    
+
+    default:
+
+        cout<<"Lo lamento papeh, no hay imagen"<< endl;
+        break;
+    }
+
+
+}
---
+++ CppServer/tarjeta.h
@@ -0,0 +1,30 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/socket.h>
+#include <stdlib.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <iostream>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <string>
+
+using namespace std;
+
+class tarjeta{
+
+public:
+
+    int pos_i, pos_j, id, memory;
+
+    string imgprovisional;
+
+    tarjeta(int pos_i, int pos_j, int id, int memory);
+
+    void obtener_imagen(int id);
+
+    void print();
+    
+
+};
+

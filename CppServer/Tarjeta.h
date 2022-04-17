ba02a3f3ecb283036b96521023e93f9192cc375d
Isac Marín Sirias <isacms11@gmail.com>
2022-04-16 22:31:25 -0600

    Revert "Revert "Envio y recepcion de peticiones""
    This reverts commit 5a85f19e1661904ee64cefc323db31c194c1c716.

--- CppServer/Tarjeta.h
+++ CppServer/Tarjeta.h
@@ -1,3 +1,6 @@
+//
+// Created by ims on 12/4/22.
+//
 
 #ifndef PRUEBAMATRIX_TARJETA_H
 #define PRUEBAMATRIX_TARJETA_H
--- CppServer/main.cpp
+++ CppServer/main.cpp
@@ -1,16 +1,32 @@
 #include <iostream>
-
+#include "Disck.h"
 #include "VirtualMatrix.h"
 #include "VirtualMatrix.cpp"
+
 
 int main(){
 
     VirtualMatrix *matrix = new VirtualMatrix();
 
-    cout<< "............................\n";
-    matrix->buscar_enMatrix(3,5);
+    Disck *disck = new Disck();
 
+    matrix->s_Matrix();
     cout<< "............................\n";
-    matrix->buscar_enMatrix(1,6);
+    matrix->buscar_enMatrix(2,2);
+    cout<< "............................\n";
+    matrix->buscar_enMatrix(1,4);
+
+
+   // cout << disck ->get_tarjeta_enDisco(1,6).image <<"\n";
+    //cout<< "............................\n";
+   // matrix->buscar_enMatrix(1,6);
+
+
+
+
+
+
+
+
 
 }
